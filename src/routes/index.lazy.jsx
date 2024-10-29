4678ff1f4885d13bc7b479a9d83e8061f40ea374
Steven Turner <sturner@forge-applications.io>
2024-10-29 17:23:30 -0500

    rebaseing

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -9,11 +9,7 @@ function Index() {
     <div className="index">
       <div className="index-brand">
         <h1>Padre Gino's</h1>
-<<<<<<< HEAD:padre-ginos/src/routes/index.lazy.jsx
         <p>Pizza & Art at a location near you</p>
-=======
-        <p>Pizza at a location near you</p>
->>>>>>> 83c710d (finishing for the day):src/routes/index.lazy.jsx
       </div>
       <ul>
         <li>
