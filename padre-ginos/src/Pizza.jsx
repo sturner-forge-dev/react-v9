666d3c9fa9db9848ec6c1d0f5748bd699adb392a
Steven Turner <sturner@forge-applications.io>
2024-10-29 11:49:39 -0500

    pizza class

---
+++ padre-ginos/src/Pizza.jsx
@@ -0,0 +1,11 @@
+// const Pizza = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("p", {}, props.description),
+//   ]);
+// };
+
+const Pizza = (props) => {
+  return <div className=""></div>;
+};
+export default Pizza;
