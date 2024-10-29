e86a667a459f08ffa7949bf6c42b3c6573c4dc3c
Steven Turner <sturner@forge-applications.io>
2024-10-29 11:34:47 -0500

    initial commit - getting started - project setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -32,6 +35,6 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 
 root.render(React.createElement(App));
