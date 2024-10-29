31e4abb5bba44690afc5f22aa9ab3bdbd3775956
Steven Turner <sturner@forge-applications.io>
2024-10-29 11:52:24 -0500

    finally got git working

--- padre-ginos/src/Pizza.jsx
+++ padre-ginos/src/Pizza.jsx
@@ -1,11 +1,9 @@
-// const Pizza = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("p", {}, props.description),
-//   ]);
-// };
-
 const Pizza = (props) => {
-  return <div className=""></div>;
+  return (
+    <div className="pizza">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
 };
 export default Pizza;
