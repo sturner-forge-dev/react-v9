2b317a797ab7d52930d87f225b9f9aed0c4a43c4
Steven Turner <sturner@forge-applications.io>
2024-10-30 13:21:42 -0500

    uncontrolled forms starting testing

---
+++ src/routes/contact.lazy.jsx
@@ -0,0 +1,47 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useMutation } from "@tanstack/react-query";
+import postContact from "../api/postContact";
+
+export const Route = createLazyFileRoute("/contact")({
+  component: ContactRoute,
+});
+
+function ContactRoute() {
+  const mutation = useMutation({
+    mutationFn: function (e) {
+      e.preventDefault();
+      const formaData = new FormData(e.target);
+
+      return postContact(
+        formaData.get("name"),
+        formaData.get("email"),
+        formaData.get("message"),
+      );
+    },
+  });
+
+  if (mutation.isError) {
+    console.error(mutation.error);
+    return <h1>Error: {mutation.error.message}</h1>;
+  }
+
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      {mutation.isSuccess ? (
+        <h3>Submitted!</h3>
+      ) : (
+        <form onSubmit={mutation.mutate}>
+          <input name="name" placeholder="Name" type="text" />
+          <input name="email" placeholder="Email" type="email" />
+          <textarea
+            name="message"
+            placeholder="Type your message"
+            type="text"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+}
