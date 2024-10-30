b3f3a793989fa0f5a62664a9312cb6911a26b3cf
Steven Turner <sturner@forge-applications.io>
2024-10-30 14:47:02 -0500

    finish testing

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
       },
-      "/public": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
+      '/public': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   },
   plugins: [TanStackRouterVite(), react()],
   test: {
-    environment: "happy-dom",
-  },
+    environment: 'happy-dom',
+    coverage: {
+      reporter: ['text', 'json', 'html']
+    }
+  }
 });
